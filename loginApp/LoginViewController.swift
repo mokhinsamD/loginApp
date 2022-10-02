e59f466a2b18fd426075dcac89f78dc5e3e5f1c5
Darina Samokhina <mokhinsam@yandex.ru>
2022-10-02 23:27:52 +0300

    set logic loginViewController and welcomeViewController

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userTextField.text
+    }
+    
 
 
+    //MARK: - IB Actions
+    @IBAction func logInButtonDidPressed() {
+        if userTextField.text != "Alexey" || passwordTextField.text != "1234" {
+            showAlert(
+                withTitle: "Invalid login or passworg",
+                andMessage: "Please, enter correct login and password."
+            )
+            passwordTextField.text = ""
+        }
+    }
+    
+    @IBAction func forgotUserNameButtonDidPressed() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is Alexey 😇")
+    }
+    
+    @IBAction func forgorPasswordButtonDidPressed() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is 1234 🤫")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let _ = segue.source as? WelcomeViewController else { return }
+        userTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+}
+
+//MARK: - UIAlertController
+extension UIViewController {
+    func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        present(alert, animated: true)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+    }
 }
 
--- loginApp/WelcomeViewController.swift
+++ loginApp/WelcomeViewController.swift
@@ -8,13 +8,19 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        greetingFor(userName)
     }
     
-
+    private func greetingFor(_ name: String) {
+        welcomeLabel.text = "Welcome, \(name)!"
+    }
 
 
 }
